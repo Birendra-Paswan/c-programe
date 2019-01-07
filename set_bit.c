62c22107a98756f92997725ebc1a7ff8c3129af2
Birendra-Paswan <40135762+Birendra-Paswan@users.noreply.github.com>
2019-01-07 23:33:40 +0530

    Add files via upload

---
+++ set_bit.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int pos,num;
+	printf("Enter the number ");
+	scanf("%d",&num);
+	printf("Enter the position ");
+	scanf("%d",&pos);
+	num=num|(1<<pos);
+	printf("%d ",num);
+}
